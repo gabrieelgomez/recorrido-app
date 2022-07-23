0c6a8aa2464784092d5f6bb5d64361536bda70c5
gabrieelgomez <gagg1707@gmail.com>
2022-07-22 20:44:58 -0400

    add employee module

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_22_234754) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_23_002133) do
   create_table "companies", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  create_table "employees", force: :cascade do |t|
+    t.string "name"
+    t.integer "company_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["company_id"], name: "index_employees_on_company_id"
+  end
+
+  add_foreign_key "employees", "companies"
 end
